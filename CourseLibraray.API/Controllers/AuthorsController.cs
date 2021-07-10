4f3b81b9edcf32ef35fcdd95d778cee81c11f718
Abhishek Sahai <abhishek.sahai@outlook.in>
2021-07-10 20:02:15 +0530

    initiali commit

---
+++ CourseLibraray.API/Controllers/AuthorsController.cs
@@ -0,0 +1,41 @@
+﻿using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace CourseLibraray.API.Controllers
+{
+    [ApiController]
+    [Route("api/authors")]
+    public class AuthorsController : ControllerBase
+    {
+        private ICourseLibraryRepository _courseLibraryRepository;
+        private ILogger<AuthorsController> _logger;
+
+        public AuthorsController(ICourseLibraryRepository courseLibraryRepository, ILogger<AuthorsController> logger)
+        {
+            _courseLibraryRepository = courseLibraryRepository ?? throw new ArgumentNullException(nameof(courseLibraryRepository));
+            _logger = logger ?? throw new ArgumentNullException(nameof(courseLibraryRepository));
+        }
+
+        [HttpGet]
+        public IActionResult Authors()
+        {
+            _logger.LogInformation(System.Reflection.MethodBase.GetCurrentMethod().Name);
+            var authors = _courseLibraryRepository.GetAuthors();
+            return Ok(authors);
+        }
+
+        [HttpGet("{authorId}")]
+        public IActionResult Authors(Guid authorId)
+        {
+            _logger.LogInformation(System.Reflection.MethodBase.GetCurrentMethod().Name);
+            var author = _courseLibraryRepository.GetAuthor(authorId);
+            if (author == null)
+            {
+                return NotFound();
+            }
+            return Ok(author);
+        }
+    }
+}
---
+++ CourseLibraray.API/Startup.cs
@@ -0,0 +1,62 @@
+using CourseLibrary.API.DbContexts;
+using CourseLibrary.API.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace CourseLibrary.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers(setupAction =>
+            {
+                setupAction.ReturnHttpNotAcceptable = true;
+            }).AddXmlDataContractSerializerFormatters();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CourseLibraray.API", Version = "v1" });
+            });
+            services.AddScoped<ICourseLibraryRepository, CourseLibraryRepository>();
+
+            services.AddDbContext<CourseLibraryContext>(options =>
+            {
+                options.UseSqlServer(
+                    @"Server=(localdb)\mssqllocaldb;Database=CourseLibraryDB;Trusted_Connection=True;");
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CourseLibraray.API v1"));
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
